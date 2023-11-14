d95b11ad1cdafbea47de35cef6ea22944659edcf
Zohanubis <phdh141003@gmail.com>
2023-11-14 17:04:35 +0700

    14/11

---
+++ Exercicse8/ThucHanhBuoi8/Class_Ex2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThucHanhBuoi8
+{
+    public partial class Class_Ex2 : Form
+    {
+        public Class_Ex2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
